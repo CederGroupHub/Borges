7f32e703a7f37fd6950e177e489449ecea6f22f4
Ziqin Rong <rongzq08@gmail.com>
2019-06-12 23:47:45 -0700

    add debug info

--- Borges/spiders/AIP/metadata.py
+++ Borges/spiders/AIP/metadata.py
@@ -64,6 +64,7 @@ class AIPIssuesSpider(scrapy.Spider):
 
     @staticmethod
     def parse_paper_meta(response):
+        print(response)
         for paper in response.css(".card-cont").getall():
             results = {}
             results.update(
